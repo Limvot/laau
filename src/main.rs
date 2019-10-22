35c1f43ed95ba9e2056cb6eddf757a2f3e7ddce5
Nathan Braswell <miloignis@gmail.com>
2019-10-21 23:32:08 -0400

    Refactor, save login

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::error::Error;
+use std::fs::File;
 use std::io::{self, Write};
 
 use reqwest;
-use serde_json::{json, Value};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 fn input(prompt: &str) -> Result<String, Box<dyn std::error::Error>> {
     print!("{}", prompt);
@@ -11,46 +14,77 @@ fn input(prompt: &str) -> Result<String, Box<dyn std::error::Error>> {
     Ok(s)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let server = input("Server: ")?.trim().to_owned();
-    let username = format!("@{}:{}", input("Username: ")?.trim(), server);
-    let password = input("Password: ")?.trim().to_owned();
+#[derive(Debug, Serialize, Deserialize)]
+struct LoginResponse {
+    access_token: String,
+    home_server: String,
+    user_id: String,
+    device_id: String,
+}
+
+fn login(server: &str, username: &str, password: &str) -> Result<LoginResponse, Box<dyn Error>> {
     let body = json!({
         "type": "m.login.password",
         "user": username,
         "password": password
     })
     .to_string();
-    println!("body is {}", body);
     let res = reqwest::Client::new()
         .post(&format!("https://{}/_matrix/client/r0/login", server))
         .body(body)
         .send()?
         .text()?;
     println!("response was {}", res);
-    let res_json: Value = serde_json::from_str(&res)?;
-    println!("json response was {:?}", res_json);
+    Ok(serde_json::from_str(&res)?)
+}
 
-    let message = input("What is your message?: ")?.trim().to_owned();
-    let room = input("What room to send to?: ")?.trim().to_owned();
-
-    let access_token = res_json["access_token"].as_str().unwrap();
-    let body = json!({
-        "msgtype": "m.text",
-        "body": message
-    })
-    .to_string();
-
+fn send_message(
+    server: &str,
+    access_token: &str,
+    room: &str,
+    msg: &str,
+) -> Result<(), Box<dyn Error>> {
     let url = format!(
         "https://{}/_matrix/client/r0/rooms/{}/send/m.room.message?access_token={}",
         server, room, access_token
     );
+    let body = json!({
+        "msgtype": "m.text",
+        "body": msg
+    })
+    .to_string();
     let res = reqwest::Client::new()
         .post(&url)
         .body(body)
         .send()?
         .text()?;
-
     println!("post response = {}", res);
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filename = "./saved_login.json";
+    let login_response = if let Ok(Ok(login_response)) =
+        File::open(filename).map(|reader| serde_json::from_reader(reader))
+    {
+        login_response
+    } else {
+        let server = input("Server: ")?.trim().to_owned();
+        let username = format!("@{}:{}", input("Username: ")?.trim(), server);
+        let password = input("Password: ")?.trim().to_owned();
+        let login_response = login(&server, &username, &password)?;
+        serde_json::to_writer(File::create(filename)?, &login_response)?;
+        login_response
+    };
+
+    let message = input("What is your message?: ")?.trim().to_owned();
+    let room = input("What room to send to?: ")?.trim().to_owned();
+
+    send_message(
+        &login_response.home_server,
+        &login_response.access_token,
+        &room,
+        &message,
+    )?;
     Ok(())
 }
